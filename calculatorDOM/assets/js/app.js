5ff9c77af58740220953ea6d7050af5f076612ad
simreteabmekbib <se.simreteab.mekbib@gmail.com>
2021-02-01 23:12:50 -0800

    make some layout change like background colour margin ....

--- calculatorDOM/assets/js/app.js
+++ calculatorDOM/assets/js/app.js
@@ -1,60 +1,3 @@
-const numberButtons = document.querySelectorAll('[data-number]')
-const operationButtons = document.querySelectorAll('[data-operation]')
-const equalsButton = document.querySelector('[data-equals]')
-const deleteButton = document.querySelector('[data-delete]')
-const allClearButton = document.querySelector('[data-all-clear]')
-const previousOperandTextElement = document.querySelector('[data-previous-operand]')
-const currentOperandTextElement = document.querySelector('[data-current-operand]')
-
-const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
-
-numberButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    calculator.appendNumber(button.innerText)
-    calculator.updateDisplay()
-  })
-})
-
-operationButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    calculator.chooseOperation(button.innerText)
-    calculator.updateDisplay()
-  })
-})
-
-equalsButton.addEventListener('click', button => {
-  calculator.compute()
-  calculator.updateDisplay()
-})
-
-allClearButton.addEventListener('click', button => {
-  calculator.clear()
-  calculator.updateDisplay()
-})
-
-deleteButton.addEventListener('click', button => {
-  calculator.delete()
-  calculator.updateDisplay()
-})
-function add(num1, num2){
-  return num1 + num2;
-}
-function sub(num1, num2){
-  return num1 - num2;
-}
-function div(num1, num2){
-  return num1 / num2;
-}
-function mul(num1, num2){
-  return num1 * num2;
-}
-function sqrt(num2){
-  return Math.sqrt(num2);
-}
-function power(num1, num2){
-  return Math.pow(num1, num2);
-}
-
 class Calculator {
   constructor(previousOperandTextElement, currentOperandTextElement) {
     this.previousOperandTextElement = previousOperandTextElement
@@ -148,4 +91,60 @@ class Calculator {
   }
 }
 
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()
+  })
+})
+
+operationButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.chooseOperation(button.innerText)
+    calculator.updateDisplay()
+  })
+})
+
+equalsButton.addEventListener('click', button => {
+  calculator.compute()
+  calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+  calculator.clear()
+  calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+  calculator.delete()
+  calculator.updateDisplay()
+})
+function add(num1, num2){
+  return num1 + num2;
+}
+function sub(num1, num2){
+  return num1 - num2;
+}
+function div(num1, num2){
+  return num1 / num2;
+}
+function mul(num1, num2){
+  return num1 * num2;
+}
+function sqrt(num2){
+  return Math.sqrt(num2);
+}
+function power(num1, num2){
+  return Math.pow(num1, num2);
+}
 
